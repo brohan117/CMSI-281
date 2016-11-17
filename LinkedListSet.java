a3bf88114435ceaa6330228dd7e1acd6fc6db781
brohan117 <rbramhe@lion.lmu.edu>
2016-11-17 09:49:02 -0800

    Add files via upload

---
+++ LinkedListSet.java
@@ -0,0 +1,75 @@
+public class LinkedList {
+
+    class Node {
+        Node next;
+        String value;
+
+        public Node(Node next, String value) {
+            this.next = next;
+            this.value = value;
+        }
+    }
+
+    // instance variables
+    Node first;
+    int n;
+
+    // construct an empty linked list
+    public LinkedList() {
+        first = null;
+        n = 0;
+    }
+
+    public void add(String s) {
+        Node newFirst = new Node(first, s);
+        first = newFirst;
+        n++;
+    }
+
+    public String removeFirst() {
+        String temp = first.value;
+        first = first.next;
+        n--;
+        return temp;
+    }
+
+    // linear search for s and remove it if it exists
+    public void removeOne(String s) {
+        Node current = first;
+        Node prev = null;
+        if (first.value.equals(s)) {
+            first = first.next;
+            n--;
+            return;
+        }
+        while (current != null) {
+            if (current.value.equals(s)) {
+                prev.next = current.next;
+                n--;
+                return;
+            }
+            prev = current;
+            current = current.next;
+        }
+    }
+
+public static void deleteDups(LinkedListNode n )
+	 { Hashtable table = new HashtableQ;
+	LinkedListNode previous = null;
+ 	while (n != null) {
+	if (table.containsKey(n.data)) {
+ previous.next = n.next;
+	}
+	 else { 
+	  	table.put(n.data, true);
+   		previous = n;
+	}
+		n = n.next; 
+		}
+    }
+
+    public void removeAll(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+}
